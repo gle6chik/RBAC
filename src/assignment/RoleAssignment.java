f8a725b931e9db42684d15fd67c3bf230fe50963
gle6chik <glebbaranov21@gmail.com>
2026-02-13 19:03:22 +0700

    1.5. Create interface RoleAssignment

---
+++ src/assignment/RoleAssignment.java
@@ -0,0 +1,13 @@
+package assignment;
+
+import model.Role;
+import model.User;
+
+public interface RoleAssignment {
+    String assignmentId();
+    User user();
+    Role role();
+    AssignmentMetadata metadata();
+    boolean isActive();
+    String assignmentType();
+}
